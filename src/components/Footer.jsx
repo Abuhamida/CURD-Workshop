fbd5fa056942c3298d5f4308fa56dc19f681349f
unknown <mohamedhabib182022@gmail.com>
2025-08-09 11:57:19 +0300

    search

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-800 text-gray-300 py-2 text-center">
+      <p className="">
+        © {new Date().getFullYear()} Task Manager. All rights reserved.
+      </p>
+    </footer>
   )
 }
